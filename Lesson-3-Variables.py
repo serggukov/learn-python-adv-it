0a390a9a9faf3b3b289cd8ee7b82cc48fa4210a8
Sergey Gukov <svgukov@alfaleasing.ru>
2021-03-15 04:40:00 +0300

    04-variables

---
+++ Lesson-3-Variables.py
@@ -0,0 +1,14 @@
+f_name = "Serg"
+l_name = "Gukov"
+fullname = f_name + " " + l_name
+age = 36
+
+print(fullname + " " + str(age))
+
+f_name = "serg"
+F_name = "SERG"
+print(f_name + "-----" + F_name)
+
+num1 = 11111111111111111111
+num2 = 22222222222222222222
+print(num1 + num2)
