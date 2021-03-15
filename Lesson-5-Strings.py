e8b65977a4cd46e96ac12abb958a63b36f5dffe8
Sergey Gukov <svgukov@alfaleasing.ru>
2021-03-15 12:36:14 +0300

    05-strings

---
+++ Lesson-5-Strings.py
@@ -0,0 +1,14 @@
+myString = "bla bla bla"
+name = "mr sERg gUKOv"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+print("strings:\n\tstr 1\n\tstr 2\n\tstr 3")
+print("----------")
+
+a = "   ...  some text ,,, ...   "
+print(a.lstrip())
+print(a.rstrip())
+print(a.strip())
+print(a.strip(', .'))
